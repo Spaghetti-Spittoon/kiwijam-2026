f1ceba163fbc81f32c4c78f99a50aa0f52c86f75
Doublecouponday <{ID}+{username}@users.noreply.github.com>
2026-07-25 06:05:47 +1200

    step: libs

--- esp32motorball/src/main.rs
+++ esp32motorball/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use esp_println::println;
+
+const SSID: &str = "";
+const PASSWORD: &str = "";
+
+#[esp_rtos::main]
+async fn main(_spawner: Spawner) -> ! {
+    println!("Connecting to WIFI...");
+
+    
 }
