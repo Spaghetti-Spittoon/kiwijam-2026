4e521172bff39c50c9b7490bee35e2ca8de46890
Doublecouponday <{ID}+{username}@users.noreply.github.com>
2026-07-25 16:44:37 +1200

    fix: only wpa2personal required

--- esp32motorball/src/wifi_connection.rs
+++ esp32motorball/src/wifi_connection.rs
@@ -1,14 +1,11 @@
 use crate::hardware_initialisation::{HardwareControls, WifiState};
 use esp_println::println;
 use esp_radio::wifi::{
-    sta::eap::{EapStationConfig, TtlsPhase2Method},
-    AuthenticationMethod, Config, ControllerConfig, WifiController,
+    sta::StationConfig, AuthenticationMethod, Config, ControllerConfig, WifiController,
 };
 
 const SSID: &str = env!("WIFI_SSID");
-const USERNAME: &str = env!("WIFI_USERNAME");
 const PASSWORD: &str = env!("WIFI_PASSWORD");
-const IDENTITY: &str = "anonymous";
 
 pub async fn connect_wifi(mut controls: HardwareControls) -> HardwareControls {
     let wifi_peripheral = match controls.wifi {
@@ -16,15 +13,12 @@ pub async fn connect_wifi(mut controls: HardwareControls) -> HardwareControls {
         WifiState::Connected(_) => return controls,
     };
 
-    let eap_config = EapStationConfig::default()
-        .with_ssid(SSID)
-        .with_auth_method(AuthenticationMethod::Wpa2Enterprise)
-        .with_identity(IDENTITY.into())
-        .with_username(USERNAME.into())
-        .with_password(PASSWORD.into())
-        .with_ttls_phase2_method(TtlsPhase2Method::Mschapv2);
-
-    let station_config = Config::EapStation(eap_config);
+    let station_config = Config::Station(
+        StationConfig::default()
+            .with_ssid(SSID)
+            .with_password(PASSWORD.into())
+            .with_auth_method(AuthenticationMethod::Wpa2Personal),
+    );
 
     println!("Starting WiFi");
 
